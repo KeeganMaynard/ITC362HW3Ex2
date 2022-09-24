de281b9f1c861a60dfb1e23bfd6c8225e63b2c80
Keegan <kgm1328@live.missouristate.edu>
2022-09-24 15:13:31 -0500

    Removed xml alterations

--- app/src/main/java/com/keegan/android/itc362hw3ex2/MainActivity.kt
+++ app/src/main/java/com/keegan/android/itc362hw3ex2/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.keegan.android.itc362hw3ex2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity{
+    private ConstraintSet set;
+    private ConstraintLayout layout;
+    private Button button3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        layout = (ConstraintLayout) findViewById(R.id.CL_1);
+
     }
 }
