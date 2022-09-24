872c6f40f9096796c235913cb56fbde324b09457
Keegan <kgm1328@live.missouristate.edu>
2022-09-24 15:09:04 -0500

    Convert MainActivity to a java base and added color strings

--- app/src/main/java/com/keegan/android/itc362hw3ex2/MainActivity.kt
+++ app/src/main/java/com/keegan/android/itc362hw3ex2/MainActivity.kt
@@ -1,11 +1,12 @@
-package com.keegan.android.itc362hw3ex2
+package com.keegan.android.itc362hw3ex2;
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 }
